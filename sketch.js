addc1a782ef89ae4e4492b00f6cec8beb470e10c
vale0815 <100883756+vale0815@users.noreply.github.com>
2022-05-17 17:48:25 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var canvas;
+var player, playerBase, playerArcher;
+var baseimage;
+
+function preload() {
+  backgroundImg = loadImage("./assets/background.png");
+  baseimage = loadImage("./assets/base.png");
+  playerimage = loadImage("./assets/player.png");
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+
+  engine = Engine.create();
+  world = engine.world;
+  angleMode(DEGREES);
+
+  
+var options 
+  isStatic = true
+}
+
+playerBase = Bodies.rectangle(200, 350, 100, 150, options);
+World.add(world, playerBase);
+
+player = Bodies.rectangle(250, player.position.y = 160, 50, 100, options);
+World.add(world, player)
+
+
+function draw() {
+  background(backgroundImg);
+
+Engine.update(engine);
+
+image(baseimage, playerBase.position.x, playerBase.position.y, 180, 150);
+image(playerImage,player.position.x,player.position.y,50,180)
+
+  // Título
+  fill("#FFFF");
+  textAlign("center");
+  textSize(40);
+  text("TIRO CON ARCO ÉPICO", width / 2, 100);
+}
